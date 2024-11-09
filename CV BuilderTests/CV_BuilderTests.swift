eacb97cf27efd6a3ca452e9df2feb222b7326314
AndriiFomichov <development.fomichov@gmail.com>
2024-11-09 16:46:49 +0200

    Initial Commit

---
+++ CV BuilderTests/CV_BuilderTests.swift
@@ -0,0 +1,17 @@
+//
+//  CV_BuilderTests.swift
+//  CV BuilderTests
+//
+//  Created by Andrii Fomichov on 09.11.2024.
+//
+
+import Testing
+@testable import CV_Builder
+
+struct CV_BuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
